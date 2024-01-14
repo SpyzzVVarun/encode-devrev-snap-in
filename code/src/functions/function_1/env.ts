d8a9c7781e4f3e8832ae597cdc10aac83b097eab
Varun Nagpal <95134445+SpyzzVVarun@users.noreply.github.com>
2024-01-14 13:57:49 +0530

    added env.ts

---
+++ code/src/functions/function_1/env.ts
@@ -0,0 +1,8 @@
+export const SENDGRID_API_KEY = "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" // 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
+export const apiURL = 'https://api.devrev.ai/rev-users.list';
+export const owner = 'SpyzzVVarun';
+export const repo = 'survey-form-deploy';
+export const surveyLink = 'https://devrev-survey-form-frontend.onrender.com';
+export const analyticsLink = "https://nps-survey-app.onrender.com/";
+export const mailSender = "notifications.devrev@gmail.com";
+export const GITHUB_AUTH = "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";
